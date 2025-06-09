bf32de286c00c96c17bee7d35b61abe6e6db9be9
3d014 <115591140+3d014@users.noreply.github.com>
2025-06-09 05:02:04 +0200

    Update SequalizeSetup.ts

--- db/SequalizeSetup.ts
+++ db/SequalizeSetup.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
 export const sequelize = new Sequelize("htec", "root", "root", {
-  host: process.env.DB_HOST_DEV,
+  host: process.env.DB_HOST,
   dialect: "mysql",
 });
